5e443a13e69059b1b4470b88dc358b11f758a66f
jwyamaguchi <jwyamaguchi@ucsd.edu>
2022-04-29 00:20:08 -0700

    commit

---
+++ skillDemo.java
@@ -0,0 +1,5 @@
+public class skillDemo {
+    public static int wordCount(String input) {
+        return input.length();
+    }
+}
---
+++ skillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void test() {
+        assertEquals(1, skillDemo.wordCount("to"));
+    }
+}
